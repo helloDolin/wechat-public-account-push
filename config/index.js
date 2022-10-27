d30356a6dd1c4376a8e2cf8322170cc5e58a00e2
Dolin <366688603@qq.com>
2022-10-27 15:00:08 +0800

    Update index.js

--- config/index.js
+++ config/index.js
@@ -91,7 +91,7 @@ export const config = {
    */
   FESTIVALS: [
     {"type": "生日", "name": "鸭梨", "year": "1991", "date": "05-04"},
-    {"type": "生日", "name": "少林", "year": "1991", "date": "01-15"},
+    {"type": "生日", "name": "Dolin", "year": "1991", "date": "01-15"},
     {"type": "节日", "name": "元旦", "year": "2023", "date": "01-01"},
     {"type": "节日", "name": "春节", "year": "2023", "date": "01-22"},
   ],
